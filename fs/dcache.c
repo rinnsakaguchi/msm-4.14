8e9f2364fa91f2a305876897793c9d1661e81af7
rinnsakaguchi <ffrizz042@gmail.com>
2025-05-27 23:57:37 +0700

    Update dcache.c

--- fs/dcache.c
+++ fs/dcache.c
@@ -2248,45 +2248,45 @@ struct dentry *__d_lookup_rcu(const struct dentry *parent,
 		 * we are still guaranteed NUL-termination of ->d_name.name.
 		 */
 		seq = raw_seqcount_begin(&dentry->d_seq);
-		if (dentry->d_parent != parent)
-			continue;
-		if (d_unhashed(dentry))
-			continue;
-		if (dentry->d_name.hash_len != hashlen)
-			continue;
-		if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
-			continue;
+        if (dentry->d_parent != parent)
+            continue;
+        if (d_unhashed(dentry))
+            continue;
+        if (dentry->d_name.hash_len != hashlen)
+            continue;
+        if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
+            continue;
 
-		if (unlikely(parent->d_flags & DCACHE_OP_COMPARE)) {
-			int tlen;
-			const char *tname;
-			if (dentry->d_name.hash != hashlen_hash(hashlen))
-				continue;
-			tlen = dentry->d_name.len;
-			tname = dentry->d_name.name;
-			/* we want a consistent (name,len) pair */
-			if (read_seqcount_retry(&dentry->d_seq, seq)) {
-				cpu_relax();
-				goto seqretry;
-			}
-			if (parent->d_op->d_compare(dentry,
-						    tlen, tname, name) != 0)
-				continue;
-		} else {
-			if (dentry->d_name.hash_len != hashlen)
-				continue;
-			if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
-				continue;
+        if (unlikely(parent->d_flags & DCACHE_OP_COMPARE)) {
+            int tlen;
+            const char *tname;
+            if (dentry->d_name.hash != hashlen_hash(hashlen))
+                continue;
+            tlen = dentry->d_name.len;
+            tname = dentry->d_name.name;
+            /* we want a consistent (name,len) pair */
+seqretry:  // Declare the label here
+            if (read_seqcount_retry(&dentry->d_seq, seq)) {
+                cpu_relax();
+                goto seqretry;  // This will now correctly jump to the label
+            }
+            if (parent->d_op->d_compare(dentry, tlen, tname, name) != 0)
+                continue;
+        } else {
+            if (dentry->d_name.hash_len != hashlen)
+                continue;
+            if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
+                continue;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
-				continue;
-			}
+            if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
+                continue;
+            }
 #endif
-		}
-		*seqp = seq;
-		return dentry;
-	}
-	return NULL;
+        }
+        *seqp = seq;
+        return dentry;
+    }
+    return NULL;
 }
 
 /**
